eaa58add0b7c5b0f26c980066ab2110d764566c1
smccord <sarah.e.mccord@gmail.com>
2016-09-13 13:07:54 -0600

    Install R Packages
    Code to add the basic suite of packages to a fresh R install.

---
+++ Install.Packages.R
@@ -0,0 +1,5 @@
+#####Install Packages####
+
+install.packages("spsurvey", "dplyr", "tidyr", "devtools", "extrafont", "foreach", "ggmap", "ggplot2", "ggthemes", "gstat", "latticeExtra", "maptools",
+                 "magrittr","markdown" , "plyr", "RColorBrewer", "rgdal", "rgeos", "rJava", "RODBC", "shiny", "snow", "xlsx", "git2r")
+
